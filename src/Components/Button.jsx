a72bdeb69131fe44cc8ac0fd1848cf4f15a073aa
Diwakar1210 <diwakar.b1999@gmail.com>
2025-01-26 09:35:35 +0530

    enabled resume download

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -8,7 +8,9 @@ const Button = () => {
       AOS.init({duration: 1000});
     }, []);
   return (
-    <a href='#' className={styles.resumeBtn} data-aos="fade-up" data-aos-delay="300">RESUME</a>
+    <a href='/diwakar-portfolio/Diwakar_UI_Developer.pdf'  target="_blank"  
+    className={styles.resumeBtn} data-aos="fade-up" data-aos-delay="300" 
+    rel="noopener noreferrer" download="Diwakar_UI_Developer.pdf">RESUME</a>
   )
 }
 
